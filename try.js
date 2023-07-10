54be761f122c2cca7ca577551e5c8abb4cac4195
Ateeb Ur Rahmaan <104502014+AteebNoOne@users.noreply.github.com>
2023-07-10 07:32:04 +0500

    check host

---
+++ try.js
@@ -0,0 +1,11 @@
+// Add this script in a separate JavaScript file, e.g., iframe-protection.js
+
+// Set the X-Frame-Options header to deny if the current website is not ateeb.com
+console.log("Host: ",window.location.host);
+if (window.location.host !== 'ateeb.com') {
+    document.addEventListener('DOMContentLoaded', function () {
+      // Set the X-Frame-Options header to deny
+      document.header = 'X-Frame-Options: DENY';
+    });
+  }
+  
